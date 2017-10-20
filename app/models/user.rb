97acbd8aba5dfe86f56b59ad35f1c3c84574a7a5
Joey R <joeyr84@gmail.com>
2017-10-19 19:57:05 -0600

    created roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  after_initialize :set_role_to_standard
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:standard, :premium, :admin]
+
+  private
+
+  def set_role_to_standard
+    self.role ||= :standard
+  end
 end
