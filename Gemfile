9c814a7ab1d18c44ad214b8b3a4306118eba373a
Joey R <joeyr84@gmail.com>
2017-10-23 20:17:16 -0600

    upgraDDe

--- Gemfile
+++ Gemfile
@@ -18,10 +18,11 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'figaro'
+gem 'figaro', '1.0'
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'pundit'
+gem 'stripe'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+  resources :charges, only: [:new, :create] do
+    collection do
+      delete 'downgrade'
+    end
+  end
+
   resources :wikis
 
   devise_for :users
-  
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
